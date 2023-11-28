ba2ca8cf2eefe7784d4fdf7135773fe66fbf365f
M4n-Co <M4nprz.H@hotmail.com>
2023-11-28 12:35:55 -0600

    intento de envio de notificaciones al enviar un mensaje

--- app/src/main/java/com/example/easychat/chat/activity/ChatActivity.kt
+++ app/src/main/java/com/example/easychat/chat/activity/ChatActivity.kt
@@ -3,19 +3,26 @@ package com.example.easychat.chat.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.AdapterDataObserver
 import com.example.easychat.chat.adapter.ChatAdapter
 import com.example.easychat.chat.model.ChatMessageModel
 import com.example.easychat.chat.model.ChatroomModel
 import com.example.easychat.databinding.ActivityChatBinding
 import com.example.easychat.model.UserModel
-import com.example.easychat.searchUser.adapterAndHolder.SearchUserAdapter
 import com.example.easychat.utils.AndroidUtil
 import com.example.easychat.utils.FirebaseUtil
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.Query
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
 class ChatActivity : AppCompatActivity() {
 
@@ -27,6 +34,7 @@ class ChatActivity : AppCompatActivity() {
     private var mChatroomModel : ChatroomModel? = null
 
     private lateinit var mAdapter : ChatAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -133,8 +141,65 @@ class ChatActivity : AppCompatActivity() {
             .addOnCompleteListener {
             if (it.isSuccessful){
                 binding.etMessage.setText("")
+                sendNotification(message)
             }
         }
 
     }
+
+    private fun sendNotification(message: String) {
+
+        FirebaseUtil().currentUserDetails().get().addOnCompleteListener {
+            if (it.isSuccessful){
+                val currentUser = it.result.toObject(UserModel::class.java)!!
+
+                try {
+                    val jsonObject = JSONObject()
+
+                    val notificationJSONObject = JSONObject()
+                    notificationJSONObject.put("title", currentUser.username)
+                    notificationJSONObject.put("body",message)
+
+                    val dataJSONObject = JSONObject()
+                    dataJSONObject.put("userId", currentUser.userId)
+
+                    jsonObject.put("notification", notificationJSONObject)
+                    jsonObject.put("data", dataJSONObject)
+                    jsonObject.put("to", mOtherUser.fcmToken)
+
+                    callApi(jsonObject)
+
+                }catch (_:Exception){
+
+                }
+            }
+        }
+
+    }
+
+    private fun callApi(jsonObject: JSONObject){
+        val json = "application/json; charset=utf-8".toMediaType()
+        val client = OkHttpClient()
+
+        val url = "https://fcm.googleapis.com/fcm/send"
+
+        val body = jsonObject.toString().toRequestBody(json)
+
+        val request = Request.Builder()
+            .url(url)
+            .patch(body)
+            .header("Authorization","Bearer AAAAYCcYYUs:APA91bGeMi9pGsdgHrKVgonxQjZVZAfzg4-o6xdUqOUYtTPaQ01oEaubreOt8FaUYpSuooSmWs-Hj42BdK_XrxGPmoGfKXx0d-idFdxmeF7TbQNJTKFY7kXSljWVN0E8HDpyu9mA_qQQ")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback{
+            override fun onFailure(call: Call, e: IOException) {
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+            }
+
+        })
+    }
+
+
 }
--- app/src/main/java/com/example/easychat/utils/AndroidUtil.kt
+++ app/src/main/java/com/example/easychat/utils/AndroidUtil.kt
@@ -9,19 +9,22 @@ class AndroidUtil {
         const val USERNAME = "USERNAME"
         const val PHONE = "PHONE"
         const val USERID = "USERID"
+        const val FCM_TOKEN = "FCM_TOKEN"
     }
 
     fun sendUser(intent: Intent, userModel: UserModel){
         intent.putExtra(USERNAME, userModel.username)
         intent.putExtra(PHONE, userModel.phone)
         intent.putExtra(USERID, userModel.userId)
+        intent.putExtra(FCM_TOKEN, userModel.fcmToken)
     }
 
     fun getUSer(intent: Intent): UserModel {
         return UserModel(
             username = intent.getStringExtra(USERNAME),
             phone = intent.getStringExtra(PHONE),
-            userId = intent.getStringExtra(USERID)
+            userId = intent.getStringExtra(USERID),
+            fcmToken = intent.getStringExtra(FCM_TOKEN)
         )
     }
 }
